66b04530aa7805d36639a5617a2a24587ae122cb
c9yu <polkj11290@naver.com>
2024-04-15 12:32:36 +0900

    20240415 오전 완료

---
+++ day03/cs03_basic_app/ex12_lambdas/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ex12_lambdas
+{
+    // 대리자를 만드는데, 정수값 두 개 받아서 정수값을 리턴해주는 함수들은 내가 대신 실행시켜줄게
+    delegate int Calculate(int a, int b); 
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("익명 메소드");
+
+            // ------------------------------------------------------------------------------------ 1. 대리자로 만든 무명 메서드
+            Calculate calc;
+            calc = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            Console.WriteLine($"계산 결과 = {calc(10,4)}");
+
+            // ------------------------------------------------------------------------------------ 2. 단일 형태의 람다식
+            // 람다식을 사용하면 훨씬 짧게 코딩이 가능하다.                                            (사용시 '=>'가 포함된다.) 리턴문도 필요가 없다.
+            // calc와 calc2는 동일한 기능을 한다.
+            Calculate calc2 = (a, b) => a + b; // (int a, int b) => a + b; // {return a + b};
+
+            Console.WriteLine($"계산 결과 = {calc(11, 4)}");
+
+            // ------------------------------------------------------------------------------------ 3. 문장 형태의 람다식, 
+            Calculate calc3 = (a, b) =>                                                             // (이 경우 리턴이 필요하다. {} 안에서는 return 생략이 불가능)
+            {
+                Console.WriteLine("이런식으로 가능합니다.");
+                var sub = a - b;
+                return sub;
+            };
+            Console.WriteLine($"계산 결과 = {calc3(11, 4)}");
+
+            // Func, Action 빌트인 대리자 사용
+            // Func 리턴값이 있기 때문에 <int>는 리턴값이 int라는 뜻
+            // <int, int>는 매개변수 하나 리턴 하나
+            Func<int> func1 = () => 10; // 10을 리턴할게
+            Console.WriteLine($"func1 = {func1()}");
+            Func<int, int> func2 = (x) => x ^ 2;
+            Console.WriteLine($"func2 = {func2(4)}");
+            Func<int, int, double> func3 = (x, y) => (double)x / y;
+            Console.WriteLine($"func3 = {func3(22, 7)}");
+
+            // Action은 리턴값이 없음
+            int result = 0;
+            Action<int> act1 = (x) => result = x * x;
+            act1(3);
+            Console.WriteLine($"act1 = {result}");
+            Action<double, double> act2 = (x, y) =>
+            {
+                double res = x / y;
+                Console.WriteLine(res);
+            };
+
+            act2(21.1, 7.0);
+        }
+    }
+    class Test
+    {
+        private int name;
+        private double point;
+
+        public int Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Point // 람다식 사용한 프롬퍼터
+        {
+            get => point;
+            set => point = value;
+        }
+    }
+}
---
+++ day03/cs03_basic_app/ex14_attributes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+
+namespace ex14_attributes
+{
+    class MyClass
+    {
+        [Obsolete("이 메서드는 다음 버전에서 폐기됩니다. NewMethod()를 사용하세요.", false)]// , true는 아예 사용 불가
+        /// <summary>
+        /// 올드메서드 이렇게 사용하세요
+        /// </summary>
+        public void OldMethod() // 최초 제작 메서드
+        {
+            Console.WriteLine("Old Method");
+        }
+
+        /// <summary>
+        /// 뉴메서드, 이렇게 사용하세요
+        /// </summary>
+        public void NewMethod() // 개선한 메서드
+        {
+            Console.WriteLine("New Method");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 리플렉션
+            Console.WriteLine("리플렉션!");
+
+            int a = int.MaxValue;
+            Type type = a.GetType();
+            Console.WriteLine(type.FullName); // System.Int32
+
+            float f = float.MaxValue;
+            Console.WriteLine(f.GetType()); // System.Single
+
+            double d = double.MaxValue;
+            Console.WriteLine(d.GetType()); // System.Double
+
+            FieldInfo[] fields = type.GetFields(); // 타입 객체에서 어떤 필드가 있는지 모두 확인
+            foreach (var item in fields)
+            {
+                Console.WriteLine($"Type : {item.FieldType}, Name : {item.Name}");
+            }
+
+            MethodInfo[] methods = type.GetMethods(); // 타입 객체에서 어떤 메서드가 있는지 모두 확인
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"Type : {item.DeclaringType}, Name : {item.Name}");
+            }
+            #endregion
+
+            // 애트리뷰트
+            Console.WriteLine("애트리뷰트!");
+
+            MyClass myClass = new MyClass();
+            myClass.OldMethod(); // 애트리뷰트로 경고를 준다.
+            myClass.NewMethod();
+        }
+    }
+}
